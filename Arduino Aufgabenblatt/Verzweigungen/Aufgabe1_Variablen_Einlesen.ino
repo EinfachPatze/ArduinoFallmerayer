cbbde2570dc166a0289e3906be9bdbeadd3ff58e
EinfachPatze <Patrick.Bergmeister@protonmail.com>
2024-10-22 11:49:03 +0200

    update for scruity

--- Arduino Aufgabenblatt/Verzweigungen/Aufgabe1_Variablen_Einlesen.ino
+++ Arduino Aufgabenblatt/Verzweigungen/Aufgabe1_Variablen_Einlesen.ino
@@ -7,10 +7,12 @@ void loop() {
   Serial.println("Gib eine Ganzzahl ein:");
   while (Serial.available() == 0) {} // Warten auf Eingabe
   int num = Serial.parseInt();
+  Serial.parseString();
 
   Serial.println("Gib eine Kommazahl ein:");
   while (Serial.available() == 0) {}
   float decimal = Serial.parseFloat();
+  Serial.parseString();
 
   Serial.println("Gib deinen Namen ein:");
   while (Serial.available() == 0) {}
